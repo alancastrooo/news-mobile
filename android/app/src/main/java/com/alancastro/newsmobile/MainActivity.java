303d1838a00231bcabff2fc961e01722a5f07e21
Alan Castro <alanarturocastrooo@gmail.com>
2026-02-24 00:31:43 -0600

    modificacion del capacitor config

---
+++ android/app/src/main/java/com/alancastro/newsmobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alancastro.newsmobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.27079005f8194ca0a9918b9cfea24198',
-  appName: 'NewsMx Tv',
+  appId: 'com.alancastro.newsmobile',
+  appName: 'NewsMxTv',
   webDir: 'dist',
-  server: {
-    url: 'https://27079005-f819-4ca0-a991-8b9cfea24198.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  bundledWebRuntime: false
 };
 
 export default config;
